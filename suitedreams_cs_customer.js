ed39961d0d7df6366017bf96ac9a0100e3ac9f8f
Timothy Johnson <tjohnson.acts1422@gmail.com>
2021-09-02 14:58:05 -0400

    initial commit

---
+++ suitedreams_cs_customer.js
@@ -0,0 +1,25 @@
+function pageInit() {
+    // alert('Hello World!'); 
+    //support email 
+    var supportEmail = nlapiGetFieldValue("custentity_sdr_support_email");
+    if (supportEmail === '') {
+        nlapiSetFieldValue("custentity_sdr_support_email", nlapiGetFieldValue('email'));
+    }
+    //coupon code
+    var applyCouponStatus = nlapiGetFieldValue("custentity_sdr_apply_coupon");
+    var couponCodeStatus = nlapiGetFieldValue("custentity_sdr_coupon_code");
+    // alert(applyCouponStatus); 
+    if (applyCouponStatus === 'T') {
+        // need to set coupon code status to enabled
+        nlapiDisableField("custentity_sdr_coupon_code", false); 
+    } else {
+        nlapiDisableField("custentity_sdr_coupon_code", true); 
+    }
+}
+
+function showSupportFrequency() {
+    var supportFreq = nlapiGetFieldText("custentity_sdr_support_frequency");
+    var internalID = nlapiGetFieldValue("custentity_sdr_support_frequency"); 
+    alert('Support frequency: ' + supportFreq + ' and internalID: ' + internalID)
+}
+
